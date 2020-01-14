8a0669acef23cf82e44a4098a36b8a9e1a062173
PeterFred <32250775+PeterFred@users.noreply.github.com>
2020-01-14 14:18:20 +1300

    Conditionally rendered the table

--- vidly-react/src/components/movies.jsx
+++ vidly-react/src/components/movies.jsx
@@ -10,37 +10,44 @@ class Movies extends Component {
   };
 
   render() {
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th />
-          </tr>
-        </thead>
+    const { length: movieCount } = this.state.movies; //Object destructuring
 
-        <tbody>
-          {this.state.movies.map(movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <button
-                  onClick={() => this.handleDelete(movie)}
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </td>
+    if (movieCount === 0) return <h4>There are no movies in the database</h4>;
+
+    return (
+      <React.Fragment>
+        <h4>Showing {movieCount} in the database</h4>;
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
