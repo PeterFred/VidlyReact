d8eaf327b0be70cd71af4b9f580a46c37cc507cc
PeterFred <32250775+PeterFred@users.noreply.github.com>
2020-01-14 13:48:01 +1300

    Added Movie compnent that renders table of movies

--- vidly-react/src/App.js
+++ vidly-react/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
+import Movies from "../src/components/movies";
 
 function App() {
   return (
     <main className="container">
-      <h1>Hello World</h1>
+      <Movies />
     </main>
   );
 }
